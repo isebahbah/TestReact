5bff2f4ed8e4784162964cc57f352e12f1ac514f
isebahbah <42175418+isebahbah@users.noreply.github.com>
2019-09-03 10:24:02 +0700

    Add files via upload

---
+++ build/precache-manifest.541ea9a8d98899aa8a40881b1c652af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c573da49d4d0d91f4ac0eee2a10c2d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83fb9501114fbb4fe5e8",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e21365e01538c7f181cf",
+    "url": "/static/css/main.faaddc3f.chunk.css"
+  },
+  {
+    "revision": "83fb9501114fbb4fe5e8",
+    "url": "/static/js/2.d904100c.chunk.js"
+  },
+  {
+    "revision": "e21365e01538c7f181cf",
+    "url": "/static/js/main.24ffdb00.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
